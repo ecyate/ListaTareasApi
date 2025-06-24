a8b81982d0f095fe51b3c23049d75c59592ca8e2
ecyate <carolinayatealape@gmail.com>
2025-06-23 22:51:55 -0500

    Agregada carpeta Data con repositorios MongoDB

---
+++ Infraestructure/Data/TareaRepository.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TareasApi.Application.Repositories;
+using TareasApi.Domain.Entities;
+
+namespace TareasApi.Infraestructure.Data
+{
+    public class TareaRepository : ITareaRepository
+    {
+        private readonly IMongoCollection<Tarea> _tareas;
+
+        public TareaRepository()
+        {
+            var context = new MongoDBContext(); // Aquí usas la clase centralizada
+            _tareas = context.GetCollection<Tarea>("Tareas");
+        }
+
+        public async Task CrearTarea(Tarea tarea)
+        {
+            await _tareas.InsertOneAsync(tarea);
+        }
+
+        public async Task EliminarTarea(string id)
+        {
+            await _tareas.DeleteOneAsync(t => t.Id == id);
+        }
+
+        public async Task<List<Tarea>> ObtenerTareasPorUsuario(string usuarioId)
+        {
+            var resultado = await _tareas.FindAsync(t => t.UsuarioId == usuarioId);
+            return await resultado.ToListAsync();
+        }
+
+        public async Task MarcarComoCompletada(string tareaId)
+        {
+            var update = Builders<Tarea>.Update.Set(t => t.Completada, true);
+            await _tareas.UpdateOneAsync(t => t.Id == tareaId, update);
+        }
+    }
+}
---
+++ Infraestructure/Data/UsuarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using MongoDB.Driver;
+using TareasApi.Domain.Entities;
+using TareasApi.Application.Repositories;
+
+namespace TareasApi.Infraestructure.Data
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly IMongoCollection<Usuario> _usuarios;
+
+        public UsuarioRepository()
+        {
+            var context = new MongoDBContext(); // Se centraliza la conexión aquí
+            _usuarios = context.GetCollection<Usuario>("Usuarios");
+        }
+
+        public async Task CrearUsuarioAsync(Usuario usuario)
+        {
+            await _usuarios.InsertOneAsync(usuario);
+        }
+
+        public async Task<Usuario?> ObtenerPorCorreoAsync(string correo)
+        {
+            var filtro = Builders<Usuario>.Filter.Eq(u => u.Correo, correo);
+            return await _usuarios.Find(filtro).FirstOrDefaultAsync();
+        }
+    }
+}
